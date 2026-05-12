e1524aecc26101986bc3e88e811b135369a2400a
ahmad-c-hub <hammoudahmad530@gmail.com>
2026-05-12 11:37:49 +0300

    switch to resend for emails

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,24 +1,17 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT),
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail({ to, subject, html }) {
   try {
-    await transporter.sendMail({
-      from: `"Hammoud Household" <${process.env.EMAIL_USER}>`,
+    const result = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
       to,
       subject,
       html,
     });
     console.log(`Email sent to ${to}`);
+    return result;
   } catch (err) {
     console.error(`Failed to send email to ${to}:`, err.message);
     throw err;
